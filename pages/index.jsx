b84785babd56e94ebaf6efe3adacc1ac5800aca4
anjaliShow <87432488+anjaliShow@users.noreply.github.com>
2022-02-17 21:02:44 +0530

    Update page title

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,18 @@ import DigitalClock from "../components/DigitalClock";
 
 const index = () => {
   return (
-    <div className="w-screen h-screen grid grid-cols-1 place-items-center bg-black">
-      <div className="text-4xl lg:text-6xl font-medium text-transparent bg-clip-text bg-gradient-to-tr from-purple-600 to-pink-600">
-        <DigitalClock />
+    <>
+      <Head>
+        <title>DigitalClock App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <div className="w-screen h-screen grid grid-cols-1 place-items-center bg-black">
+        <div className="text-4xl lg:text-6xl font-medium text-transparent bg-clip-text bg-gradient-to-tr from-purple-600 to-pink-600">
+          <DigitalClock />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
